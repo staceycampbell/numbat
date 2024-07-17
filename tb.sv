c3436d9253b066c9630ec5779f901dda44cb8468
Stacey Campbell <stacey@stacey-campbell.com>
2024-07-17 16:58:48 -0700

    Start board and pieces.

--- tb.sv
+++ tb.sv
@@ -1,8 +1,17 @@
+`include "vchess.vh"
+
 module tb;
+
+   localparam PIECE_WIDTH = `PIECE_BITS;
+   localparam ROW_WIDTH = PIECE_WIDTH * 8;
+   localparam BOARD_WIDTH = PIECE_WIDTH * 8 * 8;
 
    reg clk = 0;
    reg reset = 1;
    integer t = 0;
+   integer i;
+
+   reg [BOARD_WIDTH - 1:0] board;
 
    // should be empty
    /*AUTOREGINPUT*/
@@ -13,6 +22,27 @@ module tb;
      begin
         $dumpfile("wave.vcd");
         $dumpvars(0, tb);
+        board = 0; // wipe the board
+        board[0 * ROW_WIDTH + 0 * PIECE_WIDTH+:PIECE_WIDTH] = `WHITE_ROOK;
+        board[0 * ROW_WIDTH + 1 * PIECE_WIDTH+:PIECE_WIDTH] = `WHITE_KNIT;
+        board[0 * ROW_WIDTH + 2 * PIECE_WIDTH+:PIECE_WIDTH] = `WHITE_BISH;
+        board[0 * ROW_WIDTH + 3 * PIECE_WIDTH+:PIECE_WIDTH] = `WHITE_QUEN;
+        board[0 * ROW_WIDTH + 4 * PIECE_WIDTH+:PIECE_WIDTH] = `WHITE_KING;
+        board[0 * ROW_WIDTH + 5 * PIECE_WIDTH+:PIECE_WIDTH] = `WHITE_BISH;
+        board[0 * ROW_WIDTH + 6 * PIECE_WIDTH+:PIECE_WIDTH] = `WHITE_KNIT;
+        board[0 * ROW_WIDTH + 7 * PIECE_WIDTH+:PIECE_WIDTH] = `WHITE_ROOK;
+        for (i = 0; i < 8; i = i + 1)
+          board[1 * ROW_WIDTH + i * PIECE_WIDTH+:PIECE_WIDTH] = `WHITE_PAWN;
+        board[7 * ROW_WIDTH + 0 * PIECE_WIDTH+:PIECE_WIDTH] = `BLACK_ROOK;
+        board[7 * ROW_WIDTH + 1 * PIECE_WIDTH+:PIECE_WIDTH] = `BLACK_KNIT;
+        board[7 * ROW_WIDTH + 2 * PIECE_WIDTH+:PIECE_WIDTH] = `BLACK_BISH;
+        board[7 * ROW_WIDTH + 3 * PIECE_WIDTH+:PIECE_WIDTH] = `BLACK_QUEN;
+        board[7 * ROW_WIDTH + 4 * PIECE_WIDTH+:PIECE_WIDTH] = `BLACK_KING;
+        board[7 * ROW_WIDTH + 5 * PIECE_WIDTH+:PIECE_WIDTH] = `BLACK_BISH;
+        board[7 * ROW_WIDTH + 6 * PIECE_WIDTH+:PIECE_WIDTH] = `BLACK_KNIT;
+        board[7 * ROW_WIDTH + 7 * PIECE_WIDTH+:PIECE_WIDTH] = `BLACK_ROOK;
+        for (i = 0; i < 8; i = i + 1)
+          board[6 * ROW_WIDTH + i * PIECE_WIDTH+:PIECE_WIDTH] = `BLACK_PAWN;
         forever
           #1 clk = ~clk;
      end
---
+++ vchess.vh
@@ -0,0 +1,16 @@
+`define WHITE_PAWN 1
+`define WHITE_ROOK 2
+`define WHITE_KNIT 3
+`define WHITE_BISH 4
+`define WHITE_KING 5
+`define WHITE_QUEN 6
+
+`define BLACK_PAWN 8
+`define BLACK_ROOK 9
+`define BLACK_KNIT 10
+`define BLACK_BISH 11
+`define BLACK_KING 12
+`define BLACK_QUEN 13
+
+`define PIECE_BITS 4
+
