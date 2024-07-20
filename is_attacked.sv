4d4952a510a05f2368727cd40b36b5e7e90d2061
Stacey Campbell <stacey@stacey-campbell.com>
2024-07-20 09:57:42 -0700

    Back to encoded values for pieces. Build bitmaps using shift for is_attacked.

--- is_attacked.sv
+++ is_attacked.sv
@@ -18,57 +18,65 @@ module is_attacked #
     output reg                attacked_valid
     );
 
-   localparam ATTACK_ROOK = ATTACKER == `WHITE_ATTACK ? `WHITE_ROOK : `BLACK_ROOK;
-   localparam ATTACK_KNIT = ATTACKER == `WHITE_ATTACK ? `WHITE_KNIT : `BLACK_KNIT;
-   localparam ATTACK_BISH = ATTACKER == `WHITE_ATTACK ? `WHITE_BISH : `BLACK_BISH;
-   localparam ATTACK_QUEN = ATTACKER == `WHITE_ATTACK ? `WHITE_QUEN : `BLACK_QUEN;
-   localparam ATTACK_KING = ATTACKER == `WHITE_ATTACK ? `WHITE_KING : `BLACK_KING;
-   localparam ATTACK_PAWN = ATTACKER == `WHITE_ATTACK ? `WHITE_PAWN : `BLACK_PAWN;
+   localparam PIECE_WIDTH2 = 1 << PIECE_WIDTH;
+   localparam SIDE_WIDTH2 = PIECE_WIDTH2 * 8;
+   localparam BOARD_WIDTH2 = PIECE_WIDTH2 * 64;
 
-   reg [SIDE_WIDTH - 1:0]     rook_row_pattern [0:6];
-   reg [SIDE_WIDTH - 1:0]     rook_col_pattern [0:6];
+   localparam EMPTY_POSN2 = 1 << `EMPTY_POSN;
+   localparam ATTACK_ROOK = 1 << (ATTACKER == `WHITE_ATTACK ? `WHITE_ROOK : `BLACK_ROOK);
+   localparam ATTACK_KNIT = 1 << (ATTACKER == `WHITE_ATTACK ? `WHITE_KNIT : `BLACK_KNIT);
+   localparam ATTACK_BISH = 1 << (ATTACKER == `WHITE_ATTACK ? `WHITE_BISH : `BLACK_BISH);
+   localparam ATTACK_QUEN = 1 << (ATTACKER == `WHITE_ATTACK ? `WHITE_QUEN : `BLACK_QUEN);
+   localparam ATTACK_KING = 1 << (ATTACKER == `WHITE_ATTACK ? `WHITE_KING : `BLACK_KING);
+   localparam ATTACK_PAWN = 1 << (ATTACKER == `WHITE_ATTACK ? `WHITE_PAWN : `BLACK_PAWN);
 
-   integer                    idx, i, j;
+   localparam ATTACK_COUNT = 14;
+
+   reg [BOARD_WIDTH2 - 1:0]   attack_mask [0:ATTACK_COUNT - 1];
+
+   integer                    idx, i, j, a;
+   genvar                     gen_i;
+
+   wire [BOARD_WIDTH2 - 1:0]  board2;
 
    initial
      begin
-        $dumpfile("wave.vcd");
-        for (i = 0; i <= 6; i = i + 1)
-          begin
-             $dumpvars(0, rook_row_pattern[i]);
-             $dumpvars(0, rook_col_pattern[i]);
-          end
-        
+        //        $dumpfile("wave.vcd");
+        //        for (i = 0; i <= 6; i = i + 1)
+        //          begin
+        //          end
+        for (idx = 0; idx < ATTACK_COUNT; idx = idx + 1)
+          attack_mask[idx] = 0; // default is "don't care"
         idx = 0;
+        // rook, queen, horizontal
         for (i = 0; i < 8; i = i + 1)
           if (i != COL)
             begin
-               for (j = 0; j < 8; j = j + 1)
-                 rook_row_pattern[idx][j * PIECE_WIDTH+:PIECE_WIDTH] = 0;
-               rook_row_pattern[idx][i * PIECE_WIDTH+:PIECE_WIDTH] = ATTACK_ROOK;
-               if (i < COL)
-                 for (j = i + 1; j < COL; j = j + 1)
-                   rook_row_pattern[idx][j * PIECE_WIDTH+:PIECE_WIDTH] = `EMPTY_POSN;
-               else
-                 for (j = COL + 1; j < i; j = j + 1)
-                   rook_row_pattern[idx][j * PIECE_WIDTH+:PIECE_WIDTH] = `EMPTY_POSN;
+               attack_mask[idx][ROW * SIDE_WIDTH2 + i * PIECE_WIDTH2+:PIECE_WIDTH2] = ATTACK_ROOK | ATTACK_QUEN;
+               for (j = i + 1; j < COL; j = j + 1)
+                 attack_mask[idx][ROW * SIDE_WIDTH2 + j * PIECE_WIDTH2+:PIECE_WIDTH2] = EMPTY_POSN2;
+               for (j = COL + 1; j < i - 1; j = j + 1)
+                 attack_mask[idx][ROW * SIDE_WIDTH2 + j * PIECE_WIDTH2+:PIECE_WIDTH2] = EMPTY_POSN2;
                idx = idx + 1;
             end
-        idx = 0;
+        // rook, queen, vertical 
         for (i = 0; i < 8; i = i + 1)
           if (i != ROW)
             begin
-               for (j = 0; j < 8; j = j + 1)
-                 rook_col_pattern[idx][j * PIECE_WIDTH+:PIECE_WIDTH] = 0;
-               rook_col_pattern[idx][i * PIECE_WIDTH+:PIECE_WIDTH] = ATTACK_ROOK;
-               if (i < ROW)
-                 for (j = i + 1; j < ROW; j = j + 1)
-                   rook_col_pattern[idx][j * PIECE_WIDTH+:PIECE_WIDTH] = `EMPTY_POSN;
-               else
-                 for (j = ROW + 1; j < i; j = j + 1)
-                   rook_col_pattern[idx][j * PIECE_WIDTH+:PIECE_WIDTH] = `EMPTY_POSN;
+               attack_mask[idx][COL * PIECE_WIDTH2 + i * SIDE_WIDTH2+:PIECE_WIDTH2] = ATTACK_ROOK | ATTACK_QUEN;
+               for (j = i + 1; j < ROW; j = j + 1)
+                 attack_mask[idx][COL * PIECE_WIDTH2 + j * SIDE_WIDTH2+:PIECE_WIDTH2] = EMPTY_POSN2;
+               for (j = ROW + 1; j < i - 1; j = j + 1)
+                 attack_mask[idx][COL * PIECE_WIDTH2 + j * SIDE_WIDTH2+:PIECE_WIDTH2] = EMPTY_POSN2;
                idx = idx + 1;
             end
      end
+
+   generate
+      for (gen_i = 0; gen_i < 64; gen_i = gen_i + 1)
+        begin : bitmap_assign_blk
+           assign board2[gen_i * PIECE_WIDTH2+:PIECE_WIDTH2] = 1 << (board[gen_i * PIECE_WIDTH+:PIECE_WIDTH]);
+        end
+   endgenerate
 
 endmodule
--- vchess.vh
+++ vchess.vh
@@ -1,20 +1,20 @@
-`define EMPTY_POSN (1 << 0)
+`define EMPTY_POSN 0
 
-`define WHITE_PAWN (1 << 1)
-`define WHITE_ROOK (1 << 2)
-`define WHITE_KNIT (1 << 3)
-`define WHITE_BISH (1 << 4)
-`define WHITE_KING (1 << 5)
-`define WHITE_QUEN (1 << 6)
+`define WHITE_PAWN 1
+`define WHITE_ROOK 2
+`define WHITE_KNIT 3
+`define WHITE_BISH 4
+`define WHITE_KING 5
+`define WHITE_QUEN 6
 
-`define BLACK_PAWN (1 << 8)
-`define BLACK_ROOK (1 << 9)
-`define BLACK_KNIT (1 << 10)
-`define BLACK_BISH (1 << 11)
-`define BLACK_KING (1 << 12)
-`define BLACK_QUEN (1 << 13)
+`define BLACK_PAWN 8
+`define BLACK_ROOK 9
+`define BLACK_KNIT 10
+`define BLACK_BISH 11
+`define BLACK_KING 12
+`define BLACK_QUEN 13
 
-`define PIECE_BITS 16
+`define PIECE_BITS 4
 
 `define WHITE_ATTACK 0
 `define BLACK_ATTACK 1
