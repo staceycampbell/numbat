75219dd3d5ee37a8fff9bc03184f29783ede0057
Stacey Campbell <staceycampbell@yahoo.com>
2024-09-01 08:31:46 -0700

    Checkpoint. Problems finding the linker script.

--- vivado/src/main.c
+++ vivado/src/main.c
@@ -1,46 +1,13 @@
-/*
- * Copyright (C) 2009 - 2019 Xilinx, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
- * OF SUCH DAMAGE.
- *
- */
-
 #include <stdio.h>
-
 #include "xparameters.h"
-
 #include "netif/xadapter.h"
-
 #include "platform.h"
 #include "platform_config.h"
 #if defined (__arm__) || defined(__aarch64__)
 #include "xil_printf.h"
 #endif
-
 #include "lwip/tcp.h"
 #include "xil_cache.h"
-
 #if LWIP_IPV6==1
 #include "lwip/ip.h"
 #else
@@ -72,31 +39,28 @@ static struct netif server_netif;
 struct netif *echo_netif;
 
 #if LWIP_IPV6==1
-void print_ip6(char *msg, ip_addr_t *ip)
+void
+print_ip6(char *msg, ip_addr_t * ip)
 {
 	print(msg);
 	xil_printf(" %x:%x:%x:%x:%x:%x:%x:%x\n\r",
-			IP6_ADDR_BLOCK1(&ip->u_addr.ip6),
-			IP6_ADDR_BLOCK2(&ip->u_addr.ip6),
-			IP6_ADDR_BLOCK3(&ip->u_addr.ip6),
-			IP6_ADDR_BLOCK4(&ip->u_addr.ip6),
-			IP6_ADDR_BLOCK5(&ip->u_addr.ip6),
-			IP6_ADDR_BLOCK6(&ip->u_addr.ip6),
-			IP6_ADDR_BLOCK7(&ip->u_addr.ip6),
-			IP6_ADDR_BLOCK8(&ip->u_addr.ip6));
+		   IP6_ADDR_BLOCK1(&ip->u_addr.ip6),
+		   IP6_ADDR_BLOCK2(&ip->u_addr.ip6),
+		   IP6_ADDR_BLOCK3(&ip->u_addr.ip6),
+		   IP6_ADDR_BLOCK4(&ip->u_addr.ip6),
+		   IP6_ADDR_BLOCK5(&ip->u_addr.ip6), IP6_ADDR_BLOCK6(&ip->u_addr.ip6), IP6_ADDR_BLOCK7(&ip->u_addr.ip6), IP6_ADDR_BLOCK8(&ip->u_addr.ip6));
 
 }
 #else
 void
-print_ip(char *msg, ip_addr_t *ip)
+print_ip(char *msg, ip_addr_t * ip)
 {
 	print(msg);
-	xil_printf("%d.%d.%d.%d\n\r", ip4_addr1(ip), ip4_addr2(ip),
-			ip4_addr3(ip), ip4_addr4(ip));
+	xil_printf("%d.%d.%d.%d\n\r", ip4_addr1(ip), ip4_addr2(ip), ip4_addr3(ip), ip4_addr4(ip));
 }
 
 void
-print_ip_settings(ip_addr_t *ip, ip_addr_t *mask, ip_addr_t *gw)
+print_ip_settings(ip_addr_t * ip, ip_addr_t * mask, ip_addr_t * gw)
 {
 
 	print_ip("Board IP: ", ip);
@@ -118,15 +82,15 @@ int IicPhyReset(void);
 #endif
 #endif
 
-int main()
+int
+main()
 {
 #if LWIP_IPV6==0
 	ip_addr_t ipaddr, netmask, gw;
 
 #endif
 	/* the mac address of the board. this should be unique per board */
-	unsigned char mac_ethernet_address[] =
-	{ 0x00, 0x0a, 0x35, 0x00, 0x01, 0x02 };
+	unsigned char mac_ethernet_address[] = { 0x00, 0x0a, 0x35, 0x00, 0x01, 0x02 };
 
 	echo_netif = &server_netif;
 #if defined (__arm__) && !defined (ARMR5)
@@ -138,7 +102,8 @@ int main()
 
 /* Define this board specific macro in order perform PHY reset on ZCU102 */
 #ifdef XPS_BOARD_ZCU102
-	if(IicPhyReset()) {
+	if (IicPhyReset())
+	{
 		xil_printf("Error performing PHY reset \n\r");
 		return -1;
 	}
@@ -148,14 +113,14 @@ int main()
 
 #if LWIP_IPV6==0
 #if LWIP_DHCP==1
-    ipaddr.addr = 0;
+	ipaddr.addr = 0;
 	gw.addr = 0;
 	netmask.addr = 0;
 #else
 	/* initialize IP addresses to be used */
-	IP4_ADDR(&ipaddr,  192, 168,   1, 10);
-	IP4_ADDR(&netmask, 255, 255, 255,  0);
-	IP4_ADDR(&gw,      192, 168,   1,  1);
+	IP4_ADDR(&ipaddr, 192, 168, 0, 90);
+	IP4_ADDR(&netmask, 255, 255, 255, 0);
+	IP4_ADDR(&gw, 192, 168, 0, 1);
 #endif
 #endif
 	print_app_header();
@@ -164,16 +129,15 @@ int main()
 
 #if (LWIP_IPV6 == 0)
 	/* Add network interface to the netif_list, and set it as default */
-	if (!xemac_add(echo_netif, &ipaddr, &netmask,
-						&gw, mac_ethernet_address,
-						PLATFORM_EMAC_BASEADDR)) {
+	if (!xemac_add(echo_netif, &ipaddr, &netmask, &gw, mac_ethernet_address, PLATFORM_EMAC_BASEADDR))
+	{
 		xil_printf("Error adding N/W interface\n\r");
 		return -1;
 	}
 #else
 	/* Add network interface to the netif_list, and set it as default */
-	if (!xemac_add(echo_netif, NULL, NULL, NULL, mac_ethernet_address,
-						PLATFORM_EMAC_BASEADDR)) {
+	if (!xemac_add(echo_netif, NULL, NULL, NULL, mac_ethernet_address, PLATFORM_EMAC_BASEADDR))
+	{
 		xil_printf("Error adding N/W interface\n\r");
 		return -1;
 	}
@@ -202,16 +166,18 @@ int main()
 	dhcp_start(echo_netif);
 	dhcp_timoutcntr = 24;
 
-	while(((echo_netif->ip_addr.addr) == 0) && (dhcp_timoutcntr > 0))
+	while (((echo_netif->ip_addr.addr) == 0) && (dhcp_timoutcntr > 0))
 		xemacif_input(echo_netif);
 
-	if (dhcp_timoutcntr <= 0) {
-		if ((echo_netif->ip_addr.addr) == 0) {
+	if (dhcp_timoutcntr <= 0)
+	{
+		if ((echo_netif->ip_addr.addr) == 0)
+		{
 			xil_printf("DHCP Timeout\r\n");
-			xil_printf("Configuring default IP of 192.168.1.10\r\n");
-			IP4_ADDR(&(echo_netif->ip_addr),  192, 168,   1, 10);
-			IP4_ADDR(&(echo_netif->netmask), 255, 255, 255,  0);
-			IP4_ADDR(&(echo_netif->gw),      192, 168,   1,  1);
+			xil_printf("Configuring default IP of 192.168.0.90\r\n");
+			IP4_ADDR(&(echo_netif->ip_addr), 192, 168, 0, 90);
+			IP4_ADDR(&(echo_netif->netmask), 255, 255, 255, 0);
+			IP4_ADDR(&(echo_netif->gw), 192, 168, 0, 1);
 		}
 	}
 
@@ -227,12 +193,15 @@ int main()
 	start_application();
 
 	/* receive and process packets */
-	while (1) {
-		if (TcpFastTmrFlag) {
+	while (1)
+	{
+		if (TcpFastTmrFlag)
+		{
 			tcp_fasttmr();
 			TcpFastTmrFlag = 0;
 		}
-		if (TcpSlowTmrFlag) {
+		if (TcpSlowTmrFlag)
+		{
 			tcp_slowtmr();
 			TcpSlowTmrFlag = 0;
 		}
