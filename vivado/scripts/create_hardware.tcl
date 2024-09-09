e62333e671c36aa4bdc198bdaed156989ed07619
Stacey Campbell <staceycampbell@yahoo.com>
2024-09-09 12:56:25 -0700

    No need for performance explore just now.

--- vivado/scripts/create_hardware.tcl
+++ vivado/scripts/create_hardware.tcl
@@ -30,7 +30,7 @@ add_files -norecurse $all_src_files
 update_compile_order -fileset sources_1
 launch_runs synth_1 -jobs 8
 wait_on_run synth_1
-set_property strategy Performance_Explore [get_runs impl_1]
+# set_property strategy Performance_Explore [get_runs impl_1]
 launch_runs impl_1 -to_step write_bitstream -jobs 16
 wait_on_run impl_1
 write_hw_platform -fixed -include_bit -force -file vchess/vchess_top.xsa
